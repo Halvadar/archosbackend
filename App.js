d27669c98824f0c443b3727207b55ca7103298f9
Halvadar <Levanbaidoshvili@gmail.com>
2020-10-28 11:25:25 +0400

    'asd'

--- App.js
+++ App.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials",true)
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
