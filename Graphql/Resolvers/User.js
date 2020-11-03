90b5f740ab417014b325b00ad87472fcef9decfa
Halvadar <Levanbaidoshvili@gmail.com>
2020-11-03 18:36:35 +0400

    'asd'

--- Graphql/Resolvers/User.js
+++ Graphql/Resolvers/User.js
@@ -53,7 +53,7 @@ module.exports = {
       }
     );
 
-    res.cookie("access_token", token, { httpOnly: true,sameSite:'none' });
+    res.cookie("access_token", token, { httpOnly: true,sameSite:'none',secure:true });
     return { ...newuser._doc, usertype: "archos" };
   },
   createFacebookUser: async (args, { req, res }) => {
@@ -85,7 +85,7 @@ module.exports = {
       }
     );
 
-    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none' })
+    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none',secure:true })
 
     return { ...newuser._doc, usertype: "facebook" };
   },
@@ -113,7 +113,7 @@ module.exports = {
         expiresIn: "1d",
       }
     );
-    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none' });
+    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none',secure:true });
 
     return { ...newuser._doc, usertype: "gmail" };
   },
@@ -142,7 +142,7 @@ module.exports = {
         expiresIn: "1d",
       }
     );
-    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none' });
+    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none',secure:true });
 
     return {
       ...existingfacebookuser._doc,
@@ -170,7 +170,7 @@ module.exports = {
         expiresIn: "1d",
       }
     );
-    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none' });
+    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none',secure:true });
       
     return { ...existinggmailuser._doc, usertype: "gmail" };
   },
@@ -192,7 +192,7 @@ module.exports = {
       }
     );
 
-    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none' });
+    res.cookie("token", token, { expiresIn: "1d", httpOnly: true, sameSite:'none',secure:true });
     return { ...existinguser._doc, usertype: "archos" };
   },
   deleteUser: async (args, { req, res }) => {
